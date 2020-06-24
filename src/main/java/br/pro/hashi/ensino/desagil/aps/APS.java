123c41ab3894c1d089a2c8ac5a06cee69a54eb4b
Enrico Damiani <enrico.francesco.damiani2gmail.com>
2020-06-24 13:57:00 -0300

    inicio

--- src/main/java/br/pro/hashi/ensino/desagil/aps/APS.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/APS.java
@@ -19,7 +19,7 @@ public class APS {
         model.add(new OrGate());
         model.add(new NandGate());
         model.add(new XorGate());
-
+        model.add(new NorGate());
         // Constrói a visão, que também é um controlador.
         // Juntar visão e controlador prejudica a coesão,
         // mas decidir fazê-lo para simplificar o código.
